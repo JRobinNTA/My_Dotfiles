41bdcd3afc17c78db28c22a854d8b7bfe92675d9
Robin John <johnrobin044@gmail.com>
2025-08-06 00:26:22 +0530

    Major Fixes

--- init.lua
+++ init.lua
@@ -12,24 +12,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup {
-  require 'plugins.alpha',
-  require 'plugins.autopairs',
-  require 'plugins.blink',
-  require 'plugins.conform',
-  require 'plugins.debug',
-  require 'plugins.gitsigns',
-  require 'plugins.indent_line',
-  require 'plugins.lazydev',
-  require 'plugins.lint',
-  require 'plugins.lspconfig',
-  require 'plugins.lualine',
-  require 'plugins.mini',
-  require 'plugins.neo-tree',
-  require 'plugins.noice',
-  require 'plugins.nvim-treesitter',
-  require 'plugins.telescope',
-  require 'plugins.theme',
-  require 'plugins.toggleterm',
-  require 'plugins.which-key',
-}
+
+require('lazy').setup 'plugins'
